f7a0f6a65157b2d71de708e2ce7439826798942e
Thomas Fini Hansen <xen@xen.dk>
2015-01-24 21:13:00 +0100

    Added alias file.

---
+++ sites/all/drush/bv.aliases.drushrc.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Drrush aliases for Biblioteksvagten.
+ */
+
+// Shut up phpcs.
+$aliases = array();
+
+$aliases['vopros-server'] = array(
+  'remote-host' => 'vopros.biblioteksvagten.dk',
+  'remote-user' => 'root',
+  'path-aliases' => array(
+    '%dump-dir' => '/tmp',
+  ),
+);
+
+$aliases['vopros'] = array(
+  'parent' => '@vopros-server',
+  'uri' => 'vopros.biblioteksvagten.dk',
+  'root' => '/var/www/vopros.biblioteksvagten.dk',
+  'deployotron' => array(
+    'branch' => 'master',
+    'dump-dir' => '/home/deploy/backup/prod',
+    // Updb clears the cache.
+    'no-cc-all' => TRUE,
+  ),
+);
+
+$aliases['adm'] = array(
+  'parent' => '@vopros-server',
+  'uri' => 'adm.biblioteksvagten.dk',
+  'root' => '/var/www/adm.biblioteksvagten.dk',
+  'deployotron' => array(
+    'branch' => 'master',
+    'no-dump' => TRUE,
+    // Updb clears the cache.
+    'no-cc-all' => TRUE,
+  ),
+);
+
+$aliases['dev'] = array(
+  'parent' => '@vopros-server',
+  'uri' => 'dev.biblioteksvagten.dk',
+  'root' => '/var/www/dev.biblioteksvagten.dk',
+  'deployotron' => array(
+    'branch' => 'master',
+    'no-dump' => TRUE,
+    // Updb clears the cache.
+    'no-cc-all' => TRUE,
+  ),
+);

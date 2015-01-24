e9a3dc1f38919c1752c56bc927c73b4e9c998322
Thomas Fini Hansen <xen@xen.dk>
2015-01-24 21:28:42 +0100

    Fix aliases.

--- sites/all/drush/bv.aliases.drushrc.php
+++ sites/all/drush/bv.aliases.drushrc.php
@@ -15,22 +15,22 @@ $aliases['vopros-server'] = array(
   ),
 );
 
-$aliases['vopros'] = array(
+$aliases['adm'] = array(
   'parent' => '@vopros-server',
-  'uri' => 'vopros.biblioteksvagten.dk',
-  'root' => '/var/www/vopros.biblioteksvagten.dk',
+  'uri' => 'adm.biblioteksvagten.dk',
+  'root' => '/var/www/adm.biblioteksvagten.dk',
   'deployotron' => array(
     'branch' => 'master',
-    'dump-dir' => '/home/deploy/backup/prod',
+    'dump-dir' => '/root/backup/prod',
     // Updb clears the cache.
     'no-cc-all' => TRUE,
   ),
 );
 
-$aliases['adm'] = array(
+$aliases['vopros'] = array(
   'parent' => '@vopros-server',
-  'uri' => 'adm.biblioteksvagten.dk',
-  'root' => '/var/www/adm.biblioteksvagten.dk',
+  'uri' => 'vopros.biblioteksvagten.dk',
+  'root' => '/var/www/vopros.biblioteksvagten.dk',
   'deployotron' => array(
     'branch' => 'master',
     'no-dump' => TRUE,

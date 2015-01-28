c47e28c4f6bc243ba72ec238dbc175cfce1753ef
Thomas Fini Hansen <xen@xen.dk>
2015-01-28 11:17:46 +0100

    Commit missing profile stuff.

---
+++ profiles/vopros/modules/ting/ting_object.tpl.php
@@ -0,0 +1,15 @@
+<?php
+// $Id$
+/**
+ * @file ting_object.tpl.php
+ *
+ * Template to render objects from the Ting database.
+ *
+ * Available variables:
+ * - $object: The TingClientObject instance we're rendering.
+ * - $content: Render array of content.
+ */
+?>
+<div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+    <?php echo render($content); ?>
+</div>

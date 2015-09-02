ec7a910002e80125509a4eed8191f9ac822d15ec
Thomas Fini Hansen <xen@xen.dk>
2015-09-02 21:52:19 +0200

    Reworked tab styling. Ref BV-31.

--- sites/all/themes/watchmen/templates/vopros-embed-tab.tpl.php
+++ sites/all/themes/watchmen/templates/vopros-embed-tab.tpl.php
@@ -8,5 +8,7 @@
 <div>
   <span class="ask-vopros-icon"><img src="<?php print $logo_path; ?>"/></span>
   <span class="ask-vopros-text"><a href='<?php print $url; ?>'><?php print $title; ?></a></span>
-  <span class="ask-vopros-minimize fa fa-fw fa-times-circle"></span>
+  <span class="ask-vopros-minimize">
+    <span class="fa fa-fw fa-times-circle"></span>
+  </span>
 </div>

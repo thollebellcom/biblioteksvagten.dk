613d8bf4b588c376b57fbf013a93f6a5147cb15b
Andriy Iun <andriy.yun@gmail.com>
2018-04-20 15:11:54 +0200

    Adjust ofsets. Add comments

--- profiles/vopros/modules/vopros/vopros_embed/embedded-page.tpl.php
+++ profiles/vopros/modules/vopros/vopros_embed/embedded-page.tpl.php
@@ -40,16 +40,18 @@
 </div></div> <!-- /#page, /#page-wrapper -->
 
 <script>
-    window.onload = function() {
-      setTimeout(function() {
-        // First time visitor
-        if (! Cookies.get('firstebuster')) {
-            Cookies.set('firstebuster', 1, {expires: 1});
+  // @TODO Figureout why user ca not get correct chat button checks.
+  // Fix works by reloading page with established sesstion.
+  window.onload = function() {
+    setTimeout(function() {
+      // First time visitor
+      if (! Cookies.get('firstebuster')) {
+        Cookies.set('firstebuster', 1, {expires: 1});
 
-            if (Cookies.get('firstebuster')) {
-                window.location.reload(true);
-            }
+        if (Cookies.get('firstebuster')) {
+          window.location.reload(true);
         }
-      }, 3000);
-    };
+      }
+    }, 3000);
+  };
 </script>

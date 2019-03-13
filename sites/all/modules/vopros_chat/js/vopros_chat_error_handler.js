8d3b74b8d5136d4b161a3d3bd3e80144e6748806
Morten Nissen <mail@hjemmesidekongen.dk>
2019-03-13 15:40:17 +0100

    Uncomment chat error handler

--- sites/all/modules/vopros_chat/js/vopros_chat_error_handler.js
+++ sites/all/modules/vopros_chat/js/vopros_chat_error_handler.js
@@ -5,7 +5,7 @@
  */
 
 window.onerror = function(msg, url, line) {
-  alert("JavaScript error.\nPlease check that your browser isn't running in compatibility view.\nMessage: " + msg + "\n in " + url);
+  //alert("JavaScript error.\nPlease check that your browser isn't running in compatibility view.\nMessage: " + msg + "\n in " + url);
   // Returning true should suppress any browser error dialogs
   // (primarily old versions of IE).
   return true;

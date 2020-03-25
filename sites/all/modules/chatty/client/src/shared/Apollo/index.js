f12f2d9d774ebc6dc61a79e7935a7154b33a7579
Morten Nissen <mail@hjemmesidekongen.dk>
2020-03-25 10:25:35 +0100

    Use unfetch for older browser support

--- sites/all/modules/chatty/client/src/shared/Apollo/index.js
+++ sites/all/modules/chatty/client/src/shared/Apollo/index.js
@@ -3,20 +3,22 @@ import React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-
 import { split } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
 import { WebSocketLink } from 'apollo-link-ws';
 import { getMainDefinition } from 'apollo-utilities';
+import fetch from 'unfetch';
 
 // HTTP link.
 const httpLink = new HttpLink({
   uri: process.env.REACT_APP_APOLLO_HTTP_URI,
+  fetch: fetch,
 });
 
 // WebSocket link.
 const wsLink = new WebSocketLink({
   uri: process.env.REACT_APP_APOLLO_WS_URI,
+  fetch: fetch,
   options: {
     reconnect: true,
   },
